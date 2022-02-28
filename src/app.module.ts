7dcf119b8ef2b13d70ee06941f4dac9fbe061647
Ehsan Samavati <samaavaati@gmail.com>
2022-02-28 12:10:53 +0330

    fix: remove sensetive informations

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ChatModule } from './chat/chat.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(),
+    ChatModule,
+    UserModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
