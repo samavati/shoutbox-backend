3806257b8e272602cf3fb6829c0c87a7312cf49c
Ehsan Samavati <samaavaati@gmail.com>
2022-03-02 12:03:18 +0330

    docs: missing docs added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,13 +2,23 @@ import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from 'express-serve-static-core';
 import { join } from 'path';
+import { existsSync } from 'fs';
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   serverClient(@Res() res: Response) {
-    return res.sendFile(join(__dirname, '..', 'client'));
+    const clientFilePath = join(__dirname, '..', 'client');
+    if (existsSync(clientFilePath)) {
+      return res.sendFile(clientFilePath);
+    }
+    return res.send(`
+    <div>
+    <h1>Welcome to shoutbox</h1>
+    <p>You can use your own client application dev server or build it under client folder</p>
+    </div>
+    `)
   }
 
   @Get('config')
